0c45802e526766d61a958adf974a212ed30b0d6b
dominic <dongeun.kim@42dot.ai>
2025-04-29 12:09:21 +0900

    TAG 변수명 수정

--- AtivityLifeCycleSample/app/src/main/java/com/example/ativitylifecyclesample/MainActivity.kt
+++ AtivityLifeCycleSample/app/src/main/java/com/example/ativitylifecyclesample/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     // 로그에 사용할 태그 이름
-    private val TAG = "MainActivityLifeCycle"
+    private val tag = "MainActivityLifeCycle"
 
     // Activity 처음 생성 시 호출
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,42 +22,42 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Log.d(TAG, "onCreate is called")
+        Log.d(tag, "onCreate is called")
     }
 
     // Activity 화면 노출 시 호출
     override fun onStart() {
         super.onStart()
-        Log.d(TAG, "onStart is called")
+        Log.d(tag, "onStart is called")
     }
 
     // Activity 사용 준비 시 호출
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "onResume is called")
+        Log.d(tag, "onResume is called")
     }
 
     // 사용 중인 Activity 일시 정시 시 호출
     override fun onPause() {
         super.onPause()
-        Log.d(TAG, "onPause is called")
+        Log.d(tag, "onPause is called")
     }
 
     // Activity 가 화면 내 보이지 않을 시 호출
     override fun onStop() {
         super.onStop()
-        Log.d(TAG, "onStop is called")
+        Log.d(tag, "onStop is called")
     }
 
     // Activity 종료 시 호출 (memory 정리)
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG, "onDestroy is called")
+        Log.d(tag, "onDestroy is called")
     }
 
     // Activity 가 재 시작 시 호출
     override fun onRestart() {
         super.onRestart()
-        Log.d(TAG, "onRestart is called")
+        Log.d(tag, "onRestart is called")
     }
 }
